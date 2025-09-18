d2829e52fe22fc77b0d5797fd3b9d6a2860655d5
tig-paul <166623682+tig-paul@users.noreply.github.com>
2025-09-18 18:45:55 +0530

    updated backend service

--- Recipe Book/FrontEnd/vite.config.js
+++ Recipe Book/FrontEnd/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/server': 'http://localhost:3000'
+    },
+  },
   plugins: [tailwindcss(),react()],
 })
