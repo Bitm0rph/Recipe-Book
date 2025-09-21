381f2a9e643feedbf8a5c3bcd878e6cad13e9953
tig-paul <166623682+tig-paul@users.noreply.github.com>
2025-09-21 19:15:09 +0530

    updated cloudinary and multer services

---
+++ Recipe Book/BackEnd/src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req,file,cb) {
+        cb(null,"./public/tmp")
+    },
+    filename: function (req,file,cb){
+        // This takes 2 parameters -> error and filename
+        callback(null,file.fieldname+'_'+Date.now()+'_'+file.originalname)
+    }
+})
+
+export const upload = multer({
+    storage
+})
