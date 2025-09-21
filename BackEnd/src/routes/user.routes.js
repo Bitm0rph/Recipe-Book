b2f10be4175219cd15f85c069f7a91d9059ebde7
tig-paul <166623682+tig-paul@users.noreply.github.com>
2025-09-21 19:52:46 +0530

    updated folder structure

---
+++ BackEnd/src/routes/user.routes.js
@@ -0,0 +1,99 @@
+import express from 'express';
+import { register } from '../controllers/user.controller.js';
+
+
+const router = express.Router();
+
+router.route("/register").post(register)
+
+export default router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // register
+// router.post('/register', async (req, res) => {
+//     try {
+//         const { name, email, password } = req.body;
+//         if (!name || !email || !password) return res.status(400).json({ message: 'Please enter all fields' });
+
+
+//         const existing = await User.findOne({ email });
+//         if (existing) return res.status(400).json({ message: 'User exists' });
+
+
+//         const salt = await bcrypt.genSalt(10);
+//         const hashed = await bcrypt.hash(password, salt);
+
+
+//         const user = new User({ name, email, password: hashed });
+//         await user.save();
+
+
+//         const payload = { id: user._id };
+//         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+
+//         res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Server error');
+//     }
+// });
+
+
+// // login
+// router.post('/login', async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
+//         if (!email || !password) return res.status(400).json({ message: 'Please enter all fields' });
+
+
+//         const user = await User.findOne({ email });
+//         if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+
+//         const isMatch = await bcrypt.compare(password, user.password);
+//         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+
+//         const payload = { id: user._id };
+//         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+
+//         res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Server error');
+//     }
+// });
+
+
+// router.get("/me", auth, async (req, res) => {
+//     try {
+//         const user = await User.findById(req.user.id).select("-password"); // exclude password
+//         if (!user) return res.status(404).json({ message: "User not found" });
+
+//         res.json({user});
+//     } catch (error) {
+//         console.error("Error in /me:", error);
+//         res.status(500).json({ message: "Server error" });
+//     }
+// });
+
+
+// export default router;
---
+++ BackEnd/src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+// Note: When we create a promise using new keyword, then we take resolve and reject as parameters
+// But in this case we have to catch errors using 'catch'
+
+const asyncHandler = (fn) => {
+    return (req,res,next) => {
+        Promise
+        .resolve(fn(req,res,next))
+        // .reject((err) => next(err)) // This is wrong
+        .catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// using try-catch
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
