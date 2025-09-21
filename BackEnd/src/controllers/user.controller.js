a4f9dbbb20a3d6256e0b1a27a6efb6eeea53dd05
tig-paul <166623682+tig-paul@users.noreply.github.com>
2025-09-21 22:50:13 +0530

    managed signup route

--- BackEnd/src/controllers/user.controller.js
+++ BackEnd/src/controllers/user.controller.js
@@ -1,7 +1,60 @@
+import User from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
 
 export const register = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message: "Ok"
+    const {username, email, password} = req.body
+    console.log(username, email, password);
+
+    if(
+        [username,email,password].some(field => field?.trim() === "") 
+    ){
+        throw new ApiError(400, "All fields are required", )
+    }
+    const existedUser = User.findOne({
+        $or: [{username},{email}]
     })
+
+    if(existedUser){
+        throw new ApiError(409,"User with the username or email already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400,"Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar) {
+        throw new ApiError(500,"Server failed to upload the avatar")
+    }
+
+    // Creating the user
+    const user = await User.create({
+        username: username.toLowerCase(),
+        email,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password
+    })
+
+    // check if the user is created successfully
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500,"Internal server error")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+    )
 })
